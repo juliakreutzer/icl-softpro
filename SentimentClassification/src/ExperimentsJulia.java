87017e52913ecfeafbab614e9578424d348a9f02
juliakreutzer1 <JuliaKreutzer1@aol.com>
2013-05-16 13:46:37 +0000

    Added own Experiment Class

---
+++ SentimentClassification/src/ExperimentsJulia.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author julia
+ *
+ */
+public class ExperimentsJulia {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
