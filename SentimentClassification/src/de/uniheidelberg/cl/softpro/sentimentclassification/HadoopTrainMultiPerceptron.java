3c8167ad70e498033f95a2e20cc9263356617ab5
JuliaKreutzer1@aol.com <JuliaKreutzer1@aol.com@e71273ef-091c-334b-652a-dc6303ae1778>
2013-05-28 09:26:03 +0000

    "added two perceptron classes due to program architecture"

---
+++ SentimentClassification/src/de/uniheidelberg/cl/softpro/sentimentclassification/HadoopTrainMultiPerceptron.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package src.de.uniheidelberg.cl.softpro.sentimentclassification;
+
+/**
+ *	Trains a multi-task perceptron on Hadoop
+ *	using the 4 product categories as shards
+ */
+public class HadoopTrainMultiPerceptron {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ SentimentClassification/src/de/uniheidelberg/cl/softpro/sentimentclassification/TrainSinglePerceptron.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package src.de.uniheidelberg.cl.softpro.sentimentclassification;
+
+/**
+ * Trains a single task perceptron on training data
+ *
+ */
+public class TrainSinglePerceptron {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
