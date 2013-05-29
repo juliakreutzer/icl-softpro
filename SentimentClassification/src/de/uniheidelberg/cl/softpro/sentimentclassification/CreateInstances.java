c0624499c79f996f3bfc332b2e52d2e0840e9d9b
JuliaKreutzer1@aol.com <JuliaKreutzer1@aol.com@e71273ef-091c-334b-652a-dc6303ae1778>
2013-05-29 15:50:44 +0000

    fixed bug in createInstances -> no null pointer exceptions anymore (hopefully); TrainSinglePerceptron now works with createInstances and is ready for running!

--- SentimentClassification/src/de/uniheidelberg/cl/softpro/sentimentclassification/CreateInstances.java
+++ SentimentClassification/src/de/uniheidelberg/cl/softpro/sentimentclassification/CreateInstances.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class CreateInstances {
 	
-	public static String readFile(File f) {
+	public static ArrayList<Instance> createInstancesFromFile(File f) {
 		String line = new String();
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(f));
@@ -17,23 +17,12 @@ public class CreateInstances {
 			e.printStackTrace();
 			System.exit(1);
 		}
-		return line;
-	}
-	
-	public static ArrayList<String> formatting(String kategorie) {		//mit ArrayList
-		String[] reviews = kategorie.split("<>");
-		reviews[0] = reviews[0].replace("dvd\t", "");
-		reviews[0] = reviews[0].replace("books\t", "");
-		reviews[0] = reviews[0].replace("kitchen\t", "");
-		reviews[0] = reviews[0].replace("electronics\t", "");
+		String[] reviews = line.split("<>");
+		reviews[0] = reviews[0].replaceFirst(".*\t", "");
 		ArrayList<String> al = new ArrayList<String>();
 		for (String review : reviews) {
 			al.add(review);
 		}
-		return  al;
-	}
-	
-	public static ArrayList<Instance> makingInstances(ArrayList<String> al) {
 		ArrayList<Instance> instanceArray = new ArrayList<Instance>();
 		for (String review : al) {
 			int label = 0;
@@ -46,14 +35,46 @@ public class CreateInstances {
 			}
 			for (int i = 0; i <= reviewArray.length-2; i++) {
 				String[] keyvalue = reviewArray[i].split(":");
-				String key = keyvalue[0];
-				int value = Integer.parseInt(keyvalue[1]);
-				hm.put(key, value);
+				if (keyvalue.length == 2) {
+					String key = keyvalue[0];
+					int value = Integer.parseInt(keyvalue[1]);
+					hm.put(key, value);
+				}
 			}
 			Instance inst = new Instance(hm, label);
-			inst.foldInP(); //folds in p, i.e. adding new dimension !p with count -1
-			inst.foldInLabel(); //fold in label, i.e. new dimension !p is multiplied with label
-			
+			//System.out.println(inst.getFeatureVector());	//test
+			//System.out.println(inst.getLabel());			//test
+			instanceArray.add(inst);
+		}
+		return instanceArray;
+	}
+	
+	public static ArrayList<Instance> createInstancesFromString(String s) {
+		String[] reviews = s.split("<>");
+		reviews[0] = reviews[0].replaceFirst(".*\t", "");
+		ArrayList<String> al = new ArrayList<String>();
+		for (String review : reviews) {
+			al.add(review);
+		}
+		ArrayList<Instance> instanceArray = new ArrayList<Instance>();
+		for (String review : al) {
+			int label = 0;
+			HashMap<String, Integer> hm = new HashMap<String, Integer>();
+			String[] reviewArray = review.split(" ");
+			if (reviewArray[reviewArray.length-1].equals("#label#:negative")) {
+				label = -1;
+			} else if (reviewArray[reviewArray.length-1].equals("#label#:positive")){
+				label = 1;
+			}
+			for (int i = 0; i <= reviewArray.length-2; i++) {
+				String[] keyvalue = reviewArray[i].split(":");
+				if (keyvalue.length == 2) {
+					String key = keyvalue[0];
+					int value = Integer.parseInt(keyvalue[1]);
+					hm.put(key, value);
+				}
+			}
+			Instance inst = new Instance(hm, label);
 			//System.out.println(inst.getFeatureVector());	//test
 			//System.out.println(inst.getLabel());			//test
 			instanceArray.add(inst);
