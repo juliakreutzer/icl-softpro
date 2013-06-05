1e7458dd35901d8a2b553342ead3fe7e7ecb77a4
jmirkoh <JMirkoH@gmail.com>
2013-06-05 13:10:24 +0000

    started scaling perceptron training

--- SentimentClassification/src/de/uniheidelberg/cl/softpro/sentimentclassification/FeatureSelector.java
+++ SentimentClassification/src/de/uniheidelberg/cl/softpro/sentimentclassification/FeatureSelector.java
@@ -12,11 +12,21 @@ public class FeatureSelector {
 	HashMap <String, Double> mapWeights;
 	HashMap <String, Double> mapL2;
 	
+	/**
+	 * 
+	 * @param weights Hashmap <String, Double> containing the trained perceptron
+	 * @param l2 Hashmap <String, Double> containing the calculated l2 values
+	 */
 	public FeatureSelector (HashMap <String, Double> weights, HashMap <String, Double> l2) {
 		this.mapWeights = weights;
 		this.mapL2 = l2;
 	}
 	
+	/**
+	 * 
+	 * @param k int
+	 * @return
+	 */
 	public HashMap <String, Double> getTopKFeatures (int k) {
 		HashMap <String, Double> topFeatures = new HashMap<String, Double>();
 		Integer i = 0;
@@ -34,6 +44,11 @@ public class FeatureSelector {
 		return topFeatures;
 	}
 	
+	/**
+	 * 
+	 * @param map
+	 * @return
+	 */
 	static <K,V extends Comparable<? super V>> SortedSet<Map.Entry<K,V>> entriesSortedByValues(Map<K,V> map) {
 		SortedSet<Map.Entry<K,V>> sortedEntries = new TreeSet<Map.Entry<K,V>> (new Comparator<Map.Entry<K,V>>() {
 	            @Override public int compare(Map.Entry<K,V> e1, Map.Entry<K,V> e2) {
--- SentimentClassification/src/de/uniheidelberg/cl/softpro/sentimentclassification/TrainSinglePerceptron.java
+++ SentimentClassification/src/de/uniheidelberg/cl/softpro/sentimentclassification/TrainSinglePerceptron.java
@@ -3,7 +3,11 @@
  */
 package de.uniheidelberg.cl.softpro.sentimentclassification;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayList;
 /**
  * Trains a single task perceptron on training data
@@ -14,61 +18,68 @@ public class TrainSinglePerceptron {
 
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
-		double learningrate = 1;
-		int epochs = 10;
+	public static void main(String[] args) throws IOException {
+		double learningrate = 1.0E-4;
+		int epochs = 100;
 		
 		System.out.println(epochs+", "+learningrate);
 		
 		//creates new SingleTaskPerceptron instances
 		
 		Perceptron p = new Perceptron(epochs, learningrate);
-		Perceptron q = new Perceptron(epochs, learningrate);
-		Perceptron r = new Perceptron(epochs, learningrate);
-		Perceptron s = new Perceptron(epochs, learningrate);
-		Perceptron t = new Perceptron(epochs, learningrate);
+//		Perceptron q = new Perceptron(epochs, learningrate);
+//		Perceptron r = new Perceptron(epochs, learningrate);
+//		Perceptron s = new Perceptron(epochs, learningrate);
+//		Perceptron t = new Perceptron(epochs, learningrate);
 		
 		//reads from file to ArrayList of instances
 		//for training
-		ArrayList<Instance> train_instances_all = CreateInstances.createInstancesFromFile(new File("SentimentClassification/data/processed_acl/corpus/all.train.corpus"));
-		ArrayList<Instance> train_instances_dvd = CreateInstances.createInstancesFromFile(new File("SentimentClassification/data/processed_acl/corpus/dvd.train.corpus"));
-		ArrayList<Instance> train_instances_electronics = CreateInstances.createInstancesFromFile(new File("SentimentClassification/data/processed_acl/corpus/electronics.train.corpus"));
-		ArrayList<Instance> train_instances_kitchen = CreateInstances.createInstancesFromFile(new File("SentimentClassification/data/processed_acl/corpus/kitchen.train.corpus"));
-		ArrayList<Instance> train_instances_books = CreateInstances.createInstancesFromFile(new File("SentimentClassification/data/processed_acl/corpus/books.train.corpus"));
+		ArrayList<Instance> train_instances_all = CreateInstances.createInstancesFromFile(new File("/home/mirko/workspace/SentimentClassification/data/processed_acl/corpus/all.train.corpus"));
+//		ArrayList<Instance> train_instances_dvd = CreateInstances.createInstancesFromFile(new File("~/workspace/SentimentClassification/data/processed_acl/corpus/dvd.train.corpus"));
+//		ArrayList<Instance> train_instances_electronics = CreateInstances.createInstancesFromFile(new File("~/workspace/SentimentClassification/data/processed_acl/corpus/electronics.train.corpus"));
+//		ArrayList<Instance> train_instances_kitchen = CreateInstances.createInstancesFromFile(new File("~/workspace/SentimentClassification/data/processed_acl/corpus/kitchen.train.corpus"));
+//		ArrayList<Instance> train_instances_books = CreateInstances.createInstancesFromFile(new File("SentimentClassification/data/processed_acl/corpus/books.train.corpus"));
 		
 		//for testing
-		ArrayList<Instance>  test_instances_all = CreateInstances.createInstancesFromFile(new File("SentimentClassification/data/processed_acl/corpus/all.test.corpus"));
-		ArrayList<Instance>  test_instances_books = CreateInstances.createInstancesFromFile(new File("SentimentClassification/data/processed_acl/corpus/books.test.corpus"));
-		ArrayList<Instance>  test_instances_dvd = CreateInstances.createInstancesFromFile(new File("SentimentClassification/data/processed_acl/corpus/dvd.test.corpus"));
-		ArrayList<Instance>  test_instances_electronics = CreateInstances.createInstancesFromFile(new File("SentimentClassification/data/processed_acl/corpus/electronics.test.corpus"));
-		ArrayList<Instance>  test_instances_kitchen = CreateInstances.createInstancesFromFile(new File("SentimentClassification/data/processed_acl/corpus/kitchen.test.corpus"));
-		
+//		ArrayList<Instance>  test_instances_all = CreateInstances.createInstancesFromFile(new File("/home/mirko/workspace/SentimentClassification/data/processed_acl/corpus/all.test.corpus"));
+//		ArrayList<Instance>  test_instances_books = CreateInstances.createInstancesFromFile(new File("SentimentClassification/data/processed_acl/corpus/books.test.corpus"));
+//		ArrayList<Instance>  test_instances_dvd = CreateInstances.createInstancesFromFile(new File("SentimentClassification/data/processed_acl/corpus/dvd.test.corpus"));
+//		ArrayList<Instance>  test_instances_electronics = CreateInstances.createInstancesFromFile(new File("SentimentClassification/data/processed_acl/corpus/electronics.test.corpus"));
+//		ArrayList<Instance>  test_instances_kitchen = CreateInstances.createInstancesFromFile(new File("SentimentClassification/data/processed_acl/corpus/kitchen.test.corpus"));
+//		
 		//train perceptrons
+		System.out.println("Training...");
 		p.trainSingle(train_instances_all);
-		q.trainSingle(train_instances_books);
-		r.trainSingle(train_instances_dvd);
-		s.trainSingle(train_instances_electronics);
-		t.trainSingle(train_instances_kitchen);
+//		q.trainSingle(train_instances_books);
+//		r.trainSingle(train_instances_dvd);
+//		s.trainSingle(train_instances_electronics);
+//		t.trainSingle(train_instances_kitchen);
 	
 		//test perceptrons	
-		System.out.println("all on all: "+p.test(test_instances_all));
-		System.out.println("all on books: "+p.test(test_instances_books));
-		System.out.println("all on dvd: "+p.test(test_instances_dvd));
-		System.out.println("all on electronics: "+p.test(test_instances_electronics));
-		System.out.println("all on kitchen: "+p.test(test_instances_kitchen));
-		
-		System.out.println("books on books: "+q.test(test_instances_books));
-		System.out.println("books on all: "+q.test(test_instances_all));
-		
-		System.out.println("dvd on dvd: "+r.test(test_instances_dvd));
-		System.out.println("dvd on all: "+r.test(test_instances_all));
-		
-		System.out.println("electronics on electronics: "+s.test(test_instances_electronics));
-		System.out.println("electronics on all "+s.test(test_instances_all));
-		
-		System.out.println("kitchen on kitchen: "+t.test(test_instances_kitchen));
-		System.out.println("kitchen on all: "+t.test(test_instances_all));
+		System.out.println("Writing...");
+		BufferedWriter out = new BufferedWriter (new FileWriter ("output.txt"));
+		out.write( HadoopTrainPerceptron.convertHashMapToString (p.getWeights()));
+		out.close();
+		System.out.println("Done!");
+//		System.out.println("all on all: "+p.test(test_instances_all));
+//		System.out.println("all on books: "+p.test(test_instances_books));
+//		System.out.println("all on dvd: "+p.test(test_instances_dvd));
+//		System.out.println("all on electronics: "+p.test(test_instances_electronics));
+//		System.out.println("all on kitchen: "+p.test(test_instances_kitchen));
+//		
+//		System.out.println("books on books: "+q.test(test_instances_books));
+//		System.out.println("books on all: "+q.test(test_instances_all));
+//		
+//		System.out.println("dvd on dvd: "+r.test(test_instances_dvd));
+//		System.out.println("dvd on all: "+r.test(test_instances_all));
+//		
+//		System.out.println("electronics on electronics: "+s.test(test_instances_electronics));
+//		System.out.println("electronics on all "+s.test(test_instances_all));
+//		
+//		System.out.println("kitchen on kitchen: "+t.test(test_instances_kitchen));
+//		System.out.println("kitchen on all: "+t.test(test_instances_all));
 
 	}
 }
