6d6955bbacdbedab8dcae50c4146af639a6a2084
jasmin.schroeck@gmail.com <jasmin.schroeck@gmail.com@e71273ef-091c-334b-652a-dc6303ae1778>
2013-06-10 07:23:18 +0000

    added Evaluation to experimentsJasmin

---
+++ SentimentClassification/src/experimentsJasmin/Evaluation.java
@@ -0,0 +1,38 @@
+package src.experimentsJasmin;
+import java.io.*;
+import java.util.*;
+
+public class Evaluation {
+
+	// ist Gewichtsvektor in einer Zeile????
+	public static HashMap<String, Double> weightVectorFromFile(File f) {
+		String line = new String();
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(f));
+			line = br.readLine();
+			br.close();
+		} catch (FileNotFoundException e) {
+			System.err.println("File not found");
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		HashMap<String, Double> weightVector = new HashMap<String, Double>();
+		String[] arrayWithFeatures = line.split(" ");
+		for (String feature : arrayWithFeatures) {
+			String[] featureAndValue = feature.split(":");
+			String key = featureAndValue[0];
+			Double value = Double.parseDouble(featureAndValue[1]);
+			weightVector.put(key, value);
+		}
+		return weightVector;
+	}
+	
+	public static void main(String[] args) {
+		//System.out.println(weightVectorFromFile(new File("/home/jasmin/workspace/Test/src/testDatei.txt")));
+		HashMap<String, Double> weightVector = weightVectorFromFile(new File ("/home/jasmin/workspace/Test/src/testDatei.txt"));
+		Perceptron perceptronTest = new Perceptron(weightVector);
+		ArrayList<Instance> testset = //.....
+		perceptronTest.test(testset);
+	}
+}
