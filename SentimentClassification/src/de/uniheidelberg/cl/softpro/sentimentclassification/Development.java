e0f3e4405f74557b6e1c1257aae8009154d0112d
JuliaKreutzer1@aol.com <JuliaKreutzer1@aol.com@e71273ef-091c-334b-652a-dc6303ae1778>
2013-06-17 22:12:10 +0000

    added some basic methods for the development class

--- SentimentClassification/src/de/uniheidelberg/cl/softpro/sentimentclassification/Development.java
+++ SentimentClassification/src/de/uniheidelberg/cl/softpro/sentimentclassification/Development.java
@@ -1,5 +1,28 @@
 package de.uniheidelberg.cl.softpro.sentimentclassification;
 
+/**
+ * Class for testing on development set -> optimization of parameters
+ * @author jasmin & julia
+ */
 public class Development {
-
+	String[] epochs = {"1", "10", "100"};
+	String[] learningRates = {"exp", "dec", "1divt", "-3", "-2", "-1", "0", "1"}; //constants are exponents to power of 10 -> e.g. "0" => 1; "1" => 10
+	
+	public static void train(){
+		//trains all relevant parameter combinations and saves resulting weight vectors to files
+	}
+	
+	public static void test(){
+		//tests all relevant parameter combinations and saves resulting error rates to files
+		
+		//reads devset from file
+		//reads weight vector from file
+		//prints error rates into files
+	}
+	
+	public static void main(String[] args){
+		train();
+		test();
+	}
+	
 }
