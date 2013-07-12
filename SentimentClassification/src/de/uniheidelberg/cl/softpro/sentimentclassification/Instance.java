cc9f73fc402d371c6bb2d2795b1dcb24a393b458
juliakreutzer1 <JuliaKreutzer1@aol.com>
2013-07-12 10:58:15 +0000

    added javadoc comments for instance class

--- SentimentClassification/src/de/uniheidelberg/cl/softpro/sentimentclassification/Instance.java
+++ SentimentClassification/src/de/uniheidelberg/cl/softpro/sentimentclassification/Instance.java
@@ -4,11 +4,10 @@ import java.util.HashMap;
 import java.util.Set;
 
 /**
- * @author julia
- * objects of this class each represent one instance with feature vector and label 
+ * Objects of this class each represent one instance (-> review) with feature vector and label.
  */
 public class Instance {
-	private HashMap<String,Integer> featureVector;
+	private HashMap<String,Integer> featureVector; //contains of features and their counts
 	private int label;
 
 	/**
@@ -20,36 +19,56 @@ public class Instance {
 		this.featureVector = featureVector;
 		this.label = label;
 	}
-	
+	/**
+	 * String representation for an Instance object
+	 * @return String including feature vector and label
+	 */
 	public String toString(){
 		return "features: "+this.featureVector.toString()+", label: "+this.label;
 	}
 
+	/**
+	 * Sets the feature vector of an Instance object. 
+	 * @param newVector must be a HashMap of String and Integer values
+	 */
 	public void setFeatureVector(HashMap<String,Integer> newVector){
 		this.featureVector = newVector;
 	}
 	
+	/**
+	 * Gets an instance's feature vector.
+	 * @return HashMap of features and their counts <String, Integer>
+	 */
 	public HashMap<String, Integer> getFeatureVector() {
 		return featureVector;
 	}
 
-	
+	/**
+	 * Gets an instance's label.
+	 * @return integer value [-1,1] representing either negative or positive sentiment
+	 */
 	public int getLabel() {
 		return label;
 	}
-
-	public void setLabel(int label) {
-		this.label = label;
-	}
 	
+	/**
+	 * Gets an instance's features.
+	 * @return a set of features (Strings)
+	 */
 	public Set<String> getFeatures(){
 		return this.featureVector.keySet();
 	}
 
+	/**
+	 * For learning in modified space, an additional feature named "!p" with count -1 is added to the feature vector.
+	 */
 	public void foldInP(){
 		this.featureVector.put("!p", -1);
 	}
 	
+	/**
+	 * The instance's label is folded into its feature vector. This is needed in modified space.
+	 */
 	public void foldInLabel(){
 		this.featureVector.put("!p",this.featureVector.get("!p")*this.label);
 	}
