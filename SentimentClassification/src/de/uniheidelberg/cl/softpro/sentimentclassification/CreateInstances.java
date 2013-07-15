02391cae07af98cfccf4c48bb65cfd99ca383af7
juliakreutzer1 <JuliaKreutzer1@aol.com>
2013-07-15 14:22:10 +0000

    deleted old stuff (amazonreviews folder) and added some comments and metadata for organicfood corpus

--- SentimentClassification/src/de/uniheidelberg/cl/softpro/sentimentclassification/CreateInstances.java
+++ SentimentClassification/src/de/uniheidelberg/cl/softpro/sentimentclassification/CreateInstances.java
@@ -1,60 +1,12 @@
 package de.uniheidelberg.cl.softpro.sentimentclassification;
 
-//import Instance;
-
 import java.io.*;
 import java.util.*;
 
 /**
- * @author jasmin
  * This class provides various methods to convert the corpus text files into different formats which are needed for further work.
  */
 public class CreateInstances {
-	
-	
-	//ONLY USED FOR "OLD" HADOOP PERCEPTRON -> REMOVE IF NOT NEEDED ANYMORE
-	//liest Datei ein in Format: kat (tab) review<>review .... (alles in einer Zeile)
-	// gibt eine ArrayList mit je einer Instance pro review aus
-	public static ArrayList<Instance> createInstancesFromFile(File f) {
-		String line = new String();
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(f));
-			line = br.readLine();
-			br.close();
-		} catch (FileNotFoundException e) {
-			System.err.println("File not found");
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
-		String[] reviews = line.split("<>");
-		reviews[0] = reviews[0].replaceFirst(".*\t", "");
-		
-		ArrayList<Instance> instanceArray = new ArrayList<Instance>();
-		for (String review : reviews) {
-			int label = 0;
-			HashMap<String, Integer> hm = new HashMap<String, Integer>();
-			String[] reviewArray = review.split(" ");
-			if (reviewArray[reviewArray.length-1].equals("#label#:negative")) {
-				label = -1;
-			} else if (reviewArray[reviewArray.length-1].equals("#label#:positive")){
-				label = 1;
-			}
-			for (int i = 0; i <= reviewArray.length-2; i++) {
-				String[] keyvalue = reviewArray[i].split(":");
-				if (keyvalue.length == 2) {
-					String key = keyvalue[0];
-					int value = Integer.parseInt(keyvalue[1]);
-					hm.put(key, value);
-				}
-			}
-			Instance inst = new Instance(hm, label);
-			//System.out.println(inst.getFeatureVector());	//test
-			//System.out.println(inst.getLabel());			//test
-			instanceArray.add(inst);
-		}
-		return instanceArray;
-	}
 	
 	/**
 	 * reads text file given in following format: category (tab) feature:count feature:count #label#:positive (new line) category .... and converts it into ArrayList of Instances with one Instance per review
@@ -104,10 +56,11 @@ public class CreateInstances {
 	
 	/**
 	 * reads string of reviews given in following format: feature:count feature:count #label#:positive<>feature:count .... and converts it into ArrayList of Instances with one Instance per review
+	 * This format is only used for HadoopTrainPerceptronScalable.
 	 * @param s string to be read and converted
 	 * @return reviews as ArrayList of Instances (ArrayList<Instance>)
 	 */
-	//used in HadoopTrainPerceptronScalable and HadoopTrainPerceptron
+	//used in HadoopTrainPerceptronScalable
 	public static ArrayList<Instance> createInstancesFromString(String s) {
 		String[] reviews = s.split("<>");
 		
--- SentimentClassification/src/de/uniheidelberg/cl/softpro/sentimentclassification/WriteReview.java
+++ SentimentClassification/src/de/uniheidelberg/cl/softpro/sentimentclassification/WriteReview.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 /**
- * @author jasmin
  * This class offers the possibility to write your own reviews and tests if they are classified positive or negative. 
  */
 public class WriteReview {
@@ -17,8 +16,8 @@ public class WriteReview {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Type your review: ");
 		String review = s.nextLine();
-		review = review.replaceAll("[^a-zA-Z 0-9]", "").toLowerCase();
-		String[] reviewArray = review.split(" ");
+		review = review.replaceAll("[^a-zA-Z 0-9]", "").toLowerCase(); //convert to lower case and remove all non alphabetic or numeric characters
+		String[] reviewArray = review.split(" "); //tokenize into unigrams by simply splitting at " "
 		HashMap<String, Integer> reviewAsHashMap = new HashMap<String, Integer>();
 		for (String word : reviewArray) {
 			if (reviewAsHashMap.containsKey(word)) {
@@ -35,7 +34,7 @@ public class WriteReview {
 	 * @return label (int)
 	 */
 	public static int evaluateReview() {
-		HashMap<String, Double> weightVector = Development.weightVectorFromFile(new File("/home/mirko/workspace/SoftPro/svn/weightVectors/MTR_small.all_10_-2_5000.wv"));
+		HashMap<String, Double> weightVector = Development.weightVectorFromFile(new File("SentimentClassification/weightVectors/MTR_all_10_-2_5000.wv"));
 		HashMap<String, Integer> review = convertReviewToHashMap();
 		int label = 0;
 		if (Perceptron.dotProduct(review, weightVector) > 0) {
@@ -48,6 +47,13 @@ public class WriteReview {
 		return label;
 	}
 	
+	/**
+	 * Call this main if you want to try writing reviews.
+	 * A trained perceptron (with weight vector MTR_all_10_-2_5000.wv) analyzes your sentiment.
+	 * If the decision is unclear, "don't know" will be the feedback.
+	 * This runs in an infinite loop, so you need to interrupt the process for terminating.
+	 * @param args not needed here
+	 */
 	public static void main(String[] args) {
 	//	System.out.println(convertReviewToHashMap());
 		while(true){
