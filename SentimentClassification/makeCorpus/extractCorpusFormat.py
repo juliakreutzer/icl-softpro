151e63ef82e270427f04e4a3a5fd3e21d961af43
JMirkoH@gmail.com <JMirkoH@gmail.com@e71273ef-091c-334b-652a-dc6303ae1778>
2013-07-15 14:34:33 +0000

    Added a huge corpus :)

--- SentimentClassification/makeCorpus/extractCorpusFormat.py
+++ SentimentClassification/makeCorpus/extractCorpusFormat.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python2.7
 #-*- coding: utf-8 -*-
 import sys
 
@@ -13,11 +14,15 @@ import sys
 def extractReviews(filein):
     f = open(filein,"r")
     lines = f.readlines()
+    print "Going to read lines: "+ str(len(lines))
+    f.close
     out = []
+    nlines = 0
     for line in lines:
+        nlines += 1
         splitted = line.split('",')
         if len(splitted) < 8:
-            break
+            continue
         review = splitted[7].strip().replace('"',"")
         #print review
         title = splitted[6].strip().replace('"',"")
@@ -27,6 +32,7 @@ def extractReviews(filein):
         #tuple of review (joined text and title) and rating
         out.append((title+" "+review,rating))
    # print out
+    print str(nlines) + " lines read!"
     return out
 
 #formats each review by splitting into unigrams and counting occurrences
@@ -55,16 +61,23 @@ def countFeaturesInList(reviewList):
 #prints the reviews in final format
 def printReviewsToFile(reviewList, outfile):
     out = open(outfile,"w+")
+    counter = 0
     for review in reviewList:
         #get label from number of stars (3 stars are not used here)
-        stars = int(float(review[1]))
-        if stars<3:
-            label = "negative"
-        elif stars>3:
-            label = "positive"
-        elif stars==3:
-            continue
-        print >>out, "%s\t%s #label#:%s"%(cat," ".join(review[0]), label)
+	try:
+	        stars = int(float(review[1]))
+	        if stars<3:
+	            label = "negative"
+	        elif stars>3:
+	            label = "positive"
+	        elif stars==3:
+	            counter += 1
+	            continue
+	        print >>out, "%s\t%s #label#:%s"%(cat," ".join(review[0]), label)
+	except (ValueError):
+		counter += 1
+    print "Forgot " + str(counter) + " reviews :-("
+
         
 
 if __name__ == "__main__":
@@ -75,6 +88,7 @@ if __name__ == "__main__":
     #output file
     fileout = sys.argv[3]
     #list of reviews
+    print "Reading " + filein
     reviewList = extractReviews(filein)
     reviewFeatureCounts = countFeaturesInList(reviewList)
     printReviewsToFile(reviewFeatureCounts, fileout)
