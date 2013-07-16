7ed53931eafae06e1fac6eaa5980a38ad3f840da
JuliaKreutzer1@aol.com <JuliaKreutzer1@aol.com@e71273ef-091c-334b-652a-dc6303ae1778>
2013-07-16 16:58:40 +0000

    added ant file and changed writereview to work with ant input

--- SentimentClassification/src/de/uniheidelberg/cl/softpro/sentimentclassification/WriteReview.java
+++ SentimentClassification/src/de/uniheidelberg/cl/softpro/sentimentclassification/WriteReview.java
@@ -1,7 +1,6 @@
 package de.uniheidelberg.cl.softpro.sentimentclassification;
 import java.io.File;
 import java.util.HashMap;
-import java.util.Scanner;
 
 /**
  * This class offers the possibility to write your own reviews and tests if they are classified positive or negative. 
@@ -12,10 +11,7 @@ public class WriteReview {
 	 * reads review from standard input and converts it into a HashMap<String, Integer>
 	 * @return review (HashMap<String, Integer>)
 	 */
-	public static HashMap<String, Integer> convertReviewToHashMap() {
-		Scanner s = new Scanner(System.in);
-		System.out.println("Type your review: ");
-		String review = s.nextLine();
+	public static HashMap<String, Integer> convertReviewToHashMap(String review) {
 		review = review.replaceAll("[^a-zA-Z 0-9]", "").toLowerCase(); //convert to lower case and remove all non alphabetic or numeric characters
 		String[] reviewArray = review.split(" "); //tokenize into unigrams by simply splitting at " "
 		HashMap<String, Integer> reviewAsHashMap = new HashMap<String, Integer>();
@@ -33,9 +29,9 @@ public class WriteReview {
 	 * calculates dot product of review as HashMap and weight vector and evaluates if review is positive or negative
 	 * @return label (int)
 	 */
-	public static int evaluateReview() {
+	public static int evaluateReview(String text) {
 		HashMap<String, Double> weightVector = Development.weightVectorFromFile(new File("SentimentClassification/weightVectors/MTR_all_10_-2_5000.wv"));
-		HashMap<String, Integer> review = convertReviewToHashMap();
+		HashMap<String, Integer> review = convertReviewToHashMap(text);
 		int label = 0;
 		if (Perceptron.dotProduct(review, weightVector) > 0) {
 			label = 1;
@@ -55,18 +51,16 @@ public class WriteReview {
 	 * @param args not needed here
 	 */
 	public static void main(String[] args) {
-	//	System.out.println(convertReviewToHashMap());
-		while(true){
-			int label = evaluateReview();
-			if (label ==1){
-				System.out.println("positive!");
-			}
-			else if (label == -1){
-				System.out.println("negative!");
-			}
-			else {
-				System.out.println("don't know ...");
-			}
+		String text = args[0];
+		int label = evaluateReview(text);
+		if (label ==1){
+			System.out.println("positive!");
+		}
+		else if (label == -1){
+			System.out.println("negative!");
+		}
+		else {
+			System.out.println("don't know ...");
 		}
 	}
 }
